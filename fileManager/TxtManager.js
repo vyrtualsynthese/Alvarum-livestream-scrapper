fc17e4a5b50ef6e1e28def962509145a2e3ad1f7
Valentin Ronteix <vyrtualsynthese@gmail.com>
2019-03-09 19:51:56 +0100

    Mon tchat est trop fort !

--- fileManager/TxtManager.js
+++ fileManager/TxtManager.js
@@ -1,3 +1,41 @@
+const fs = require('fs');
+const fsPromise = require('fs').promises;
+
+const donationGoalFile = './donationGoal.txt';
+
 module.exports = class TxtManager {
 
+    //useless but I like it !
+    async fileOpener(){
+        let filehandle;
+        try {
+            filehandle = await fsPromise.open(donationGoalFile, 'r');
+        } finally {
+            if (filehandle !== undefined) {
+                await filehandle.close();
+            } else {
+                console.log('unable to reach donationGoal.txt');
+                process.exit(22);
+            }
+        }
+    };
+
+    async fileReader() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(donationGoalFile, 'utf8', (err, fileContent) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(fileContent);
+            })
+        });
+    }
+
+    fileWriter(donationGoal) {
+        fs.writeFile(donationGoalFile, donationGoal, 'utf8', (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+    }
 };
--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-console.log("Hello le monde !");
+const TxtManager = require('./fileManager/TxtManager');
 
+let currentGoal;
+
+//#### Application initializer should run only on time at start.
+
+const txtManager = new TxtManager();
+// Verify if the file exists and is writable
+txtManager.fileOpener()
+    .then(
+        () => {
+            return txtManager.fileReader()
+        })
+    .then(
+        (fileContent) =>
+        {
+            console.log(fileContent);
+        });
+
+// Read the content from the file and verify if it is a numer. If it is a number then put that in a buffer variable if not set buffer variable to Zero.
+
+//#### This part should run in loop during application execution.
+txtManager.fileWriter('Je suis la premiere donation');
+txtManager.fileWriter('Je suis la seconde donation');
